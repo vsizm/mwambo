7016ad5f1df6636cf023b5c89fa2890c51e5589b
vsizm <vsiprojectszm@gmail.com>
2026-09-21 19:01:47 +0200

    fix: load editorial styles from root layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,5 +1,4 @@
 import { requireEditorialAccess } from "../../lib/editorial-auth";
-import "../../simple-admin.css";
 
 export default async function AdminLayout({ children }: { children: React.ReactNode }) {
   await requireEditorialAccess();
