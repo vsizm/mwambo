b24acb9800dca099f75fdf5346494cc5adfa777d
vsizm <vsiprojectszm@gmail.com>
2026-09-22 22:34:08 +0200

    add marriage readiness page

---
+++ src/app/marriage-family/readiness/page.tsx
@@ -0,0 +1,5 @@
+import MarriageReadinessAssessment from "./MarriageReadinessAssessment";
+
+export const metadata={title:"Marriage Readiness & Preparedness"};
+
+export default function MarriageReadinessPage(){return <MarriageReadinessAssessment/>}
