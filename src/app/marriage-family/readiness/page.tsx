304714bedbc540cd260c11d66bd23ef6710897e9
vsizm <vsiprojectszm@gmail.com>
2026-09-23 18:31:41 +0200

    Apply readiness advisory flow to direct assessment route

--- src/app/marriage-family/readiness/page.tsx
+++ src/app/marriage-family/readiness/page.tsx
@@ -2,4 +2,6 @@ import MarriageReadinessAssessment from "./MarriageReadinessAssessment";
 
 export const metadata={title:"Marriage Readiness & Preparedness"};
 
-export default function MarriageReadinessPage(){return <MarriageReadinessAssessment/>}
+export default function MarriageReadinessPage(){
+  return <MarriageReadinessAssessment popup />;
+}
